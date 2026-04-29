c4609d47290a834a31ef438a706108b792989232
Rohit Kshatriya <97273307+Rohit280903@users.noreply.github.com>
2026-04-30 02:06:22 +0530

    Time: 1 ms (89.04%), Space: 19.4 MB (3.42%) - LeetHub

--- 3427-sum-of-variable-length-subarrays/3427-sum-of-variable-length-subarrays.py
+++ 3427-sum-of-variable-length-subarrays/3427-sum-of-variable-length-subarrays.py
@@ -1,8 +1,17 @@
+# class Solution:
+#     def subarraySum(self, nums: List[int]) -> int:
+#         res = 0
+#         for i in range(len(nums)):
+#             start = max(0, i - nums[i])
+#             for j in range(start, i+1):
+#                 res += nums[j]
+#         return res
 class Solution:
     def subarraySum(self, nums: List[int]) -> int:
-        res = 0
-        for i in range(len(nums)):
-            start = max(0, i - nums[i])
-            for j in range(start, i+1):
-                res += nums[j]
+        res = nums[0]
+        for i in range(1, len(nums)):
+            j = i - nums[i] - 1
+            nums[i] += nums[i - 1]
+            res += nums[i] - (0 if j < 0 else nums[j])
         return res
+    
